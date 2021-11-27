44670fcbd3b9d292d6fdfc28914637aed3c69037
SmileyTrek <57508671+SmileyTrek@users.noreply.github.com>
2021-11-26 21:01:43 -0600

    Add project files from original project on replit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+Author       : Miles Regal, Anthony Garufi
+Class        : CSI-140 : Intro to Programming
+Assignment   : Final Project
+Date Assigned: 11/08/21
+Due Date     : 12/06/21
+
+Description:
+             A banking system for tellers.
+
+Certification of Authenticity:
+
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking
+service (which may then retain a copy of this assignment on its
+database for the purpose of future plagiarism checking)
+
+*/
+#include <iostream>
+#include <iomanip> // setw(), precision()
+#include <fstream>
+#include <regex> // "regex" object,
+
+
+
+int main()
+{
+  return 0;
+}
